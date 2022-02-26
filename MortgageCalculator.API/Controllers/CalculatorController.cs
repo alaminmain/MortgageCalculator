952966fd320f3294ea58f04e35d30388ec578a42
Al-Amin Hossain <alaminmain@gmail.com>
2022-02-26 11:40:04 +0600

    Add Comment

--- MortgageCalculator.API/Controllers/CalculatorController.cs
+++ MortgageCalculator.API/Controllers/CalculatorController.cs
@@ -3,23 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using MortgageCalculator.API.Models.BLL;
 using MortgageCalculator.API.Models.DAO;
 
-namespace MortgageCalculator.API.Controllers
-{
-    [Route("api/[controller]")]
+namespace MortgageCalculator.API.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class CalculatorController : ControllerBase
-    {
+    public class CalculatorController : ControllerBase {
+        /// <summary>
+        /// Get payment per Payment Schedule
+        /// </summary>
+        /// <param name="input">From Query get Calculator Object</param>
+        /// <returns>Get Double Result about payment per payment Schedule</returns>
         [HttpGet]
-        public ActionResult<double> GetPaymentPerPaymentSchedule([FromQuery] CalculatorObj input)
-        {
-            if (ModelState.IsValid)
-            {
-                var ans = new PaymentCalculator();
-                return ans.CalculatePaymentPerPaymentSchedule(input);
-            }
-            else
-            {
-                return BadRequest();
+        public ActionResult<double> GetPaymentPerPaymentSchedule ([FromQuery] CalculatorObj input) {
+            if (ModelState.IsValid) {
+                var payment = new PaymentCalculator ();
+                return payment.CalculatePaymentPerPaymentSchedule (input);
+            } else {
+                return BadRequest ();
             }
         }
     }
